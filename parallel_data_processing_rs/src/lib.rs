118619e35c37472f655aa95fea949e512fd48b5d
YamiYume <p.steven.t.g@gmail.com>
2022-11-27 20:15:40 -0500

    Final Version

--- parallel_data_processing_rs/src/lib.rs
+++ parallel_data_processing_rs/src/lib.rs
@@ -1,9 +1,27 @@
+#![feature(portable_simd)]
+use ndarray::s;
 use numpy::PyReadonlyArray1;
 use pyo3::prelude::*;
 use std::iter::repeat;
+use std::mem::size_of;
+use std::slice;
 use std::thread;
 
+#[cfg(target_arch = "x86_64")]
+use std::arch::x86_64;
+use std::arch::x86_64::__m256d;
+
+use std::simd::f64x4;
+
 const CORES: usize = 6;
+
+trait SimdLen {
+    const SIMD_LEN: usize;
+}
+
+impl SimdLen for f64 {
+    const SIMD_LEN: usize = 32 / size_of::<f64>();
+}
 
 #[pymodule]
 fn parallel_data_processing_rs(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
@@ -13,6 +31,12 @@ fn parallel_data_processing_rs(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(stdev, m)?)?;
     m.add_function(wrap_pyfunction!(max, m)?)?;
     m.add_function(wrap_pyfunction!(min, m)?)?;
+    m.add_function(wrap_pyfunction!(sum_simd, m)?)?;
+    m.add_function(wrap_pyfunction!(mean_simd, m)?)?;
+    m.add_function(wrap_pyfunction!(var_simd, m)?)?;
+    m.add_function(wrap_pyfunction!(stdev_simd, m)?)?;
+    m.add_function(wrap_pyfunction!(max_simd, m)?)?;
+    m.add_function(wrap_pyfunction!(min_simd, m)?)?;
     Ok(())
 }
 
@@ -23,9 +47,21 @@ fn sum(arr: PyReadonlyArray1<f64>) -> PyResult<f64> {
 }
 
 #[pyfunction]
+fn sum_simd(arr: PyReadonlyArray1<f64>) -> PyResult<f64> {
+    let arr = arr.to_owned_array();
+    Ok(sum_arr_simd(&arr))
+}
+
+#[pyfunction]
 fn mean(arr: PyReadonlyArray1<f64>) -> PyResult<f64> {
     let arr = arr.to_owned_array();
     Ok(mean_arr(&arr))
+}
+
+#[pyfunction]
+fn mean_simd(arr: PyReadonlyArray1<f64>) -> PyResult<f64> {
+    let arr = arr.to_owned_array();
+    Ok(mean_arr_simd(&arr))
 }
 
 #[pyfunction]
@@ -35,9 +71,21 @@ fn var(arr: PyReadonlyArray1<f64>) -> PyResult<f64> {
 }
 
 #[pyfunction]
+fn var_simd(arr: PyReadonlyArray1<f64>) -> PyResult<f64> {
+    let arr = arr.to_owned_array();
+    Ok(var_arr_simd(&arr))
+}
+
+#[pyfunction]
 fn stdev(arr: PyReadonlyArray1<f64>) -> PyResult<f64> {
     let arr = arr.to_owned_array();
     Ok(stdev_arr(&arr))
+}
+
+#[pyfunction]
+fn stdev_simd(arr: PyReadonlyArray1<f64>) -> PyResult<f64> {
+    let arr = arr.to_owned_array();
+    Ok(stdev_arr_simd(&arr))
 }
 
 #[pyfunction]
@@ -47,9 +95,21 @@ fn max(arr: PyReadonlyArray1<f64>) -> PyResult<f64> {
 }
 
 #[pyfunction]
+fn max_simd(arr: PyReadonlyArray1<f64>) -> PyResult<f64> {
+    let arr = arr.to_owned_array();
+    Ok(max_arr_simd(&arr))
+}
+
+#[pyfunction]
 fn min(arr: PyReadonlyArray1<f64>) -> PyResult<f64> {
     let arr = arr.to_owned_array();
     Ok(min_arr(&arr))
+}
+
+#[pyfunction]
+fn min_simd(arr: PyReadonlyArray1<f64>) -> PyResult<f64> {
+    let arr = arr.to_owned_array();
+    Ok(min_arr_simd(&arr))
 }
 
 fn sum_arr(arr: &ndarray::Array1<f64>) -> f64 {
@@ -58,13 +118,12 @@ fn sum_arr(arr: &ndarray::Array1<f64>) -> f64 {
     }
 
     let indexes = indexing_reps(arr.len());
-    let arr = arr.as_ptr() as usize;
+    let arr_ptr = arr.as_ptr() as usize;
     let mut handles = Vec::with_capacity(CORES);
-    for (i, rep) in indexes.iter().enumerate() {
-        let rep = *rep;
-        let local_ptr = arr.clone();
+
+    for (i, &rep) in indexes.iter().enumerate() {
         let handle = thread::spawn(move || {
-            let local_ptr = unsafe{ (local_ptr as *const f64).add(i) };
+            let local_ptr = unsafe { (arr_ptr as *const f64).add(i) };
             let mut sum = 0.0;
             for r in 0..rep {
                 sum += unsafe { *local_ptr.add(CORES * r) }
@@ -81,8 +140,51 @@ fn sum_arr(arr: &ndarray::Array1<f64>) -> f64 {
     sum
 }
 
+fn sum_arr_simd(arr: &ndarray::Array1<f64>) -> f64 {
+    if arr.len() < CORES * f64::SIMD_LEN {
+        return arr.sum();
+    }
+
+    let (reps, leftover) = chunking_reps(&arr);
+    let arr_ptr = arr.as_ptr() as usize;
+    let mut handles = Vec::with_capacity(CORES);
+
+    for (i, &rep) in reps.iter().enumerate() {
+        let handle = thread::spawn(move || {
+            let local_ptr = unsafe { (arr_ptr as *const f64).add(i * f64::SIMD_LEN) };
+            let mut vsum = unsafe { x86_64::_mm256_setzero_pd() };
+
+            for r in 0..rep {
+                let chunk = unsafe {
+                    slice::from_raw_parts(local_ptr.add(CORES * f64::SIMD_LEN * r), f64::SIMD_LEN)
+                };
+                let chunk = __m256d::from(f64x4::from_slice(chunk));
+                vsum = unsafe { x86_64::_mm256_add_pd(vsum, chunk) };
+            }
+
+            let vsum_128l = unsafe { x86_64::_mm256_castpd256_pd128(vsum) };
+            let vsum_128h = unsafe { x86_64::_mm256_extractf128_pd::<1>(vsum) };
+            let vwsum = unsafe { x86_64::_mm_add_pd(vsum_128l, vsum_128h) };
+            let vwsum_64h = unsafe { x86_64::_mm_unpackhi_pd(vwsum, vwsum) };
+            unsafe { x86_64::_mm_cvtsd_f64(x86_64::_mm_add_sd(vwsum, vwsum_64h)) }
+        });
+        handles.push(handle);
+    }
+
+    let mut sum: f64 = arr.slice(s![arr.len() - leftover..]).iter().sum();
+    for handle in handles {
+        sum += handle.join().expect("Error Joining a thread");
+    }
+
+    sum
+}
+
 fn mean_arr(arr: &ndarray::Array1<f64>) -> f64 {
     sum_arr(&arr) / arr.len() as f64
+}
+
+fn mean_arr_simd(arr: &ndarray::Array1<f64>) -> f64 {
+    sum_arr_simd(&arr) / arr.len() as f64
 }
 
 fn var_arr(arr: &ndarray::Array1<f64>) -> f64 {
@@ -90,18 +192,16 @@ fn var_arr(arr: &ndarray::Array1<f64>) -> f64 {
         return arr.var(0.0);
     }
 
-    let len = arr.len() as f64;
-    let mean = mean_arr(arr);
+    let mean = mean_arr(&arr);
 
     let indexes = indexing_reps(arr.len());
-    let arr = arr.as_ptr() as usize;
+    let arr_ptr = arr.as_ptr() as usize;
     let mut handles = Vec::with_capacity(CORES);
 
-    for (i, rep) in indexes.iter().enumerate() {
-        let rep = *rep;
-        let local_ptr = arr.clone();
+    for (i, &rep) in indexes.iter().enumerate() {
+        let local_ptr = arr_ptr.clone();
         let handle = thread::spawn(move || {
-            let local_ptr = unsafe{ (local_ptr as *const f64).add(i) };
+            let local_ptr = unsafe { (local_ptr as *const f64).add(i) };
             let mut sum = 0.0;
             for r in 0..rep {
                 sum += (unsafe { *local_ptr.add(CORES * r) } - mean).powi(2);
@@ -115,11 +215,59 @@ fn var_arr(arr: &ndarray::Array1<f64>) -> f64 {
     for handle in handles {
         sum += handle.join().expect("Error Joining a thread");
     }
-    sum / len
+    sum / arr.len() as f64
+}
+
+fn var_arr_simd(arr: &ndarray::Array1<f64>) -> f64 {
+    if arr.len() < CORES * f64::SIMD_LEN {
+        return arr.var(0.0);
+    }
+    let mean = mean_arr_simd(&arr);
+    let (indexes, leftover) = chunking_reps(&arr);
+    let arr_ptr = arr.as_ptr() as usize;
+    let mut handles = Vec::with_capacity(CORES);
+    for (i, &rep) in indexes.iter().enumerate() {
+        let handle = thread::spawn(move || {
+            let mut local_ptr = arr_ptr as *const f64;
+            local_ptr = unsafe { local_ptr.add(i * f64::SIMD_LEN) };
+            let local_mean = unsafe { x86_64::_mm256_set1_pd(mean) };
+            let mut vsum = unsafe { x86_64::_mm256_setzero_pd() };
+            for r in 0..rep {
+                let chunk = unsafe {
+                    slice::from_raw_parts(local_ptr.add(CORES * f64::SIMD_LEN * r), f64::SIMD_LEN)
+                };
+                let mut chunk = __m256d::from(f64x4::from_slice(chunk));
+                chunk = unsafe { x86_64::_mm256_sub_pd(chunk, local_mean) };
+                chunk = unsafe { x86_64::_mm256_mul_pd(chunk, chunk) };
+                vsum = unsafe { x86_64::_mm256_add_pd(vsum, chunk) };
+            }
+            let vsum_128l = unsafe { x86_64::_mm256_castpd256_pd128(vsum) };
+            let vsum_128h = unsafe { x86_64::_mm256_extractf128_pd::<1>(vsum) };
+            let vwsum = unsafe { x86_64::_mm_add_pd(vsum_128l, vsum_128h) };
+            let vwsum_64h = unsafe { x86_64::_mm_unpackhi_pd(vwsum, vwsum) };
+            unsafe { x86_64::_mm_cvtsd_f64(x86_64::_mm_add_sd(vwsum, vwsum_64h)) }
+        });
+        handles.push(handle);
+    }
+
+    let mut sum: f64 = arr
+        .slice(s![arr.len() - leftover..])
+        .iter()
+        .map(|x| (x - mean).powi(2))
+        .sum();
+    for handle in handles {
+        sum += handle.join().expect("Error Joining a thread");
+    }
+
+    sum / arr.len() as f64
 }
 
 fn stdev_arr(arr: &ndarray::Array1<f64>) -> f64 {
     var_arr(&arr).sqrt()
+}
+
+fn stdev_arr_simd(arr: &ndarray::Array1<f64>) -> f64 {
+    var_arr_simd(&arr).sqrt()
 }
 
 fn max_arr(arr: &ndarray::Array1<f64>) -> f64 {
@@ -139,7 +287,7 @@ fn max_arr(arr: &ndarray::Array1<f64>) -> f64 {
         let rep = *rep;
         let local_ptr = arr.clone();
         let handle = thread::spawn(move || {
-            let local_ptr = unsafe{ (local_ptr as *const f64).add(i) };
+            let local_ptr = unsafe { (local_ptr as *const f64).add(i) };
             let mut new_max = f64::MIN;
             for r in 0..rep {
                 if new_max < unsafe { *local_ptr.add(CORES * r) } {
@@ -161,6 +309,55 @@ fn max_arr(arr: &ndarray::Array1<f64>) -> f64 {
     return new_max;
 }
 
+fn max_arr_simd(arr: &ndarray::Array1<f64>) -> f64 {
+    if arr.len() < CORES * f64::SIMD_LEN {
+        return arr.iter().copied().reduce(f64::max).unwrap();
+    }
+
+    let (reps, leftover) = chunking_reps(&arr);
+    let arr_ptr = arr.as_ptr() as usize;
+    let mut handles = Vec::with_capacity(CORES);
+
+    for (i, &rep) in reps.iter().enumerate() {
+        let handle = thread::spawn(move || {
+            let local_ptr = unsafe { (arr_ptr as *const f64).add(i * f64::SIMD_LEN) };
+            let mut vmax = unsafe { x86_64::_mm256_set1_pd(f64::MIN) };
+
+            for r in 0..rep {
+                let chunk = unsafe {
+                    slice::from_raw_parts(local_ptr.add(CORES * f64::SIMD_LEN * r), f64::SIMD_LEN)
+                };
+                let chunk = __m256d::from(f64x4::from_slice(chunk));
+                vmax = unsafe { x86_64::_mm256_max_pd(vmax, chunk) };
+            }
+
+            let mut pvmax = unsafe { x86_64::_mm256_permute2f128_pd(vmax, vmax, 1) };
+            vmax = unsafe { x86_64::_mm256_max_pd(vmax, pvmax) };
+            pvmax = unsafe { x86_64::_mm256_permute_pd::<5>(vmax) };
+            vmax = unsafe { x86_64::_mm256_max_pd(vmax, pvmax) };
+            let vmax = unsafe { x86_64::_mm256_extractf128_pd::<0>(vmax) };
+            unsafe { x86_64::_mm_cvtsd_f64(vmax) }
+        });
+        handles.push(handle);
+    }
+
+    let mut max: f64 = arr
+        .slice(s![arr.len() - leftover..])
+        .iter()
+        .copied()
+        .reduce(f64::max)
+        .unwrap_or(f64::MIN);
+
+    for handle in handles {
+        let candidate = handle.join().expect("Error Joining a thread");
+        if candidate > max {
+            max = candidate
+        }
+    }
+
+    max
+}
+
 fn min_arr(arr: &ndarray::Array1<f64>) -> f64 {
     if arr.len() < CORES {
         let mut new_min = f64::MAX;
@@ -178,7 +375,7 @@ fn min_arr(arr: &ndarray::Array1<f64>) -> f64 {
         let rep = *rep;
         let local_ptr = arr.clone();
         let handle = thread::spawn(move || {
-            let local_ptr = unsafe{ (local_ptr as *const f64).add(i) };
+            let local_ptr = unsafe { (local_ptr as *const f64).add(i) };
             let mut new_min = f64::MAX;
             for r in 0..rep {
                 if new_min > unsafe { *local_ptr.add(CORES * r) } {
@@ -200,6 +397,55 @@ fn min_arr(arr: &ndarray::Array1<f64>) -> f64 {
     return new_min;
 }
 
+fn min_arr_simd(arr: &ndarray::Array1<f64>) -> f64 {
+    if arr.len() < CORES * f64::SIMD_LEN {
+        return arr.iter().copied().reduce(f64::min).unwrap();
+    }
+
+    let (reps, leftover) = chunking_reps(&arr);
+    let arr_ptr = arr.as_ptr() as usize;
+    let mut handles = Vec::with_capacity(CORES);
+
+    for (i, &rep) in reps.iter().enumerate() {
+        let handle = thread::spawn(move || {
+            let local_ptr = unsafe { (arr_ptr as *const f64).add(i * f64::SIMD_LEN) };
+            let mut vmin = unsafe { x86_64::_mm256_set1_pd(f64::MAX) };
+
+            for r in 0..rep {
+                let chunk = unsafe {
+                    slice::from_raw_parts(local_ptr.add(CORES * f64::SIMD_LEN * r), f64::SIMD_LEN)
+                };
+                let chunk = __m256d::from(f64x4::from_slice(chunk));
+                vmin = unsafe { x86_64::_mm256_min_pd(vmin, chunk) };
+            }
+
+            let mut pvmin = unsafe { x86_64::_mm256_permute2f128_pd(vmin, vmin, 1) };
+            vmin = unsafe { x86_64::_mm256_min_pd(vmin, pvmin) };
+            pvmin = unsafe { x86_64::_mm256_permute_pd::<5>(vmin) };
+            vmin = unsafe { x86_64::_mm256_min_pd(vmin, pvmin) };
+            let vmin = unsafe { x86_64::_mm256_extractf128_pd::<0>(vmin) };
+            unsafe { x86_64::_mm_cvtsd_f64(vmin) }
+        });
+        handles.push(handle);
+    }
+
+    let mut min: f64 = arr
+        .slice(s![arr.len() - leftover..])
+        .iter()
+        .copied()
+        .reduce(f64::min)
+        .unwrap_or(f64::MAX);
+
+    for handle in handles {
+        let candidate = handle.join().expect("Error Joining a thread");
+        if candidate < min {
+            min = candidate
+        }
+    }
+
+    min
+}
+
 fn indexing_reps(size: usize) -> Vec<usize> {
     let d = size / CORES;
     let r = size % CORES;
@@ -207,4 +453,18 @@ fn indexing_reps(size: usize) -> Vec<usize> {
         .take(r)
         .chain(repeat(d).take(CORES - r))
         .collect()
+}
+
+fn chunking_reps<T: SimdLen>(arr: &ndarray::Array1<T>) -> (Vec<usize>, usize) {
+    let chunk_quantity = arr.len() / T::SIMD_LEN;
+    let leftover = arr.len() % T::SIMD_LEN;
+    let d = chunk_quantity / CORES;
+    let r = chunk_quantity % CORES;
+    (
+        repeat(d + 1)
+            .take(r)
+            .chain(repeat(d).take(CORES - r))
+            .collect(),
+        leftover,
+    )
 }
